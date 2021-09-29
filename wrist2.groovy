91b417ccf53654e3e1a4269003ede998e0e76c54
Kevin Harrington <kevin.harrington@halodi.com>
2021-09-29 16:03:05 -0400

    adding wrist generators

---
+++ wrist2.groovy
@@ -0,0 +1,16 @@
+import com.neuronrobotics.bowlerstudio.creature.MobileBaseLoader
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase
+import com.neuronrobotics.sdk.common.DeviceManager
+
+if(args==null) {
+	MobileBase base=DeviceManager.getSpecificDevice( "CarlTheWalkingRobot",{
+		//If the device does not exist, prompt for the connection
+		
+		MobileBase m = MobileBaseLoader.fromGit(
+			"https://github.com/Halloween2020TheChild/GroguMechanicsCad.git",
+			"hephaestus.xml"
+			)
+		return m
+	})
+	args = [base.getAllDHChains().get(0),4]
+}
